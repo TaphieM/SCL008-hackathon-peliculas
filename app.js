10f7424a7cb667f460d679bb6b5ecebf049f694f
TaphieM <taffymayen@gmail.com>
2019-03-21 11:17:35 -0600

    se borraron funciones que no sirve

--- app.js
+++ app.js
@@ -12,18 +12,41 @@ const actionData= () =>{
     
        document.getElementById('init').innerHTML += 
 
-       `<div class="col-md-2">\
-       <div class="card"">\
+       `<div class="col-md-4">\
+       <div class="card" style="width: 18rem;">\
        <img src="${data.Poster}" class="card-img-top" alt="...">\
        <div class="card-body">\
          <h5 class="card-title">${data.Title}</h5>\
         <span class="card-text">Calificación: ${data.imdbRating}</span>\
        </div>\
+       <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
     
        </div>
   
        </div>\
-     </div>`
+     </div>
+     <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>`
     })
     }
 } 
