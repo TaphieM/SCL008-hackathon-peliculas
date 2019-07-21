08e3196266bb8fd100080683dbd80b99258c4956
TaphieM <taffymayen@gmail.com>
2019-07-21 09:41:39 -0500

    se modificaron links de icono y de carrusel

--- main.js
+++ main.js
@@ -31,70 +31,70 @@ let firstScreen =  '\
             </ol>\
             <div class="carousel-inner">\
               <div class="carousel-item active">\
-                <img class="d-block mx-auto" style="width: 900px; height:450px;" src="http://img.fenixzone.net/i/wQ3BxJh.jpeg" alt="slide 1">\
+                <img class="d-block mx-auto" style="width: 900px; height:450px;" src="https://i1.wp.com/frasesdelapelicula.com/wp-content/uploads/2010/10/el-joven-manos-de-tijera.jpg?fit=1200%2C630&ssl=1" alt="slide 1">\
                 <div class="carousel-caption d-none d-md-block" id="carouseltext">\
                     <h5>El joven manos de tijera</h5>\
                     <p>(1990)</p>\
                   </div>\
               </div>\
               <div class="carousel-item">\
-                <img class="d-block mx-auto" style="width: 900px; height:450px;" src="http://img.fenixzone.net/i/xXGtYQK.png" alt="slide 2">\
+                <img class="d-block mx-auto" style="width: 900px; height:450px;" src="https://i1.wp.com/frasesdelapelicula.com/wp-content/uploads/2011/05/el-silencio-de-los-inocentes-1.jpg?fit=1200%2C630&ssl=1" alt="slide 2">\
                 <div class="carousel-caption d-none d-md-block" id="carouseltext">\
                     <h5>El silencio de los inocentes</h5>\
                     <p>(1991)</p>\
                   </div>\
               </div>\
               <div class="carousel-item">\
-                <img class="d-block mx-auto" style="width: 900px; height:450px;" src="http://img.fenixzone.net/i/S1Cq70Z.jpeg" alt="slide 3">\
+                <img class="d-block mx-auto" style="width: 900px; height:450px;" src="https://i1.wp.com/frasesdelapelicula.com/wp-content/uploads/2012/02/forrest-gump.jpg?w=1200&ssl=1 alt="slide 3">\
                 <div class="carousel-caption d-none d-md-block" id="carouseltext">\
                     <h5>Forrest Gump</h5>\
                     <p>(1994)</p>\
                   </div>\
               </div>\
             <div class="carousel-item">\
-                <img class="d-block mx-auto" style="width: 1000px; height:450px;" src="http://img.fenixzone.net/i/gEvWsTX.jpeg" alt="slide 4">\
+                <img class="d-block mx-auto" style="width: 1000px; height:450px;" src="https://img.europapress.es/fotoweb/fotonoticia_20190301134220_640.jpg" alt="slide 4">\
                 <div class="carousel-caption d-none d-md-block" id="carouseltext">\
                     <h5>La lista de Schindler</h5>\
                     <p>(1993)</p>\
                   </div>\
               </div>\
                   <div class="carousel-item">\
-                  <img class="d-block mx-auto" style="width: 900px; height:450px;" src="http://img.fenixzone.net/i/rKFUQ4a.jpeg" alt="slide 5">\
+                  <img class="d-block mx-auto" style="width: 900px; height:450px;" src="https://junkee.com/wp-content/uploads/2019/02/Jurassic-Park-2.jpg" alt="slide 5">\
                   <div class="carousel-caption d-none d-md-block" id="carouseltext">\
                       <h5>Jurassic Park</h5>\
                       <p>(1993)</p>\
                     </div>\
                 </div>\
                 <div class="carousel-item">\
-                    <img class="d-block mx-auto" style="width: 900px; height:450px;" src="http://img.fenixzone.net/i/lQaZVfG.jpeg" alt="slide 6">\
+                    <img class="d-block mx-auto" style="width: 900px; height:450px;" src="https://www.biography.com/.image/t_share/MTI4ODc4MjY1NzI4Mjk4OTc4/pretty_woman_1jpg.jpg" alt="slide 6">\
                     <div class="carousel-caption d-none d-md-block" id="carouseltext">\
                         <h5>Mujer Bonita</h5>\
                         <p>(1990)</p>\
                       </div>\
                   </div>\
                   <div class="carousel-item">\
-                      <img class="d-block mx-auto" style="width: 900px; height:450px;" src="http://img.fenixzone.net/i/HGa2WPK.jpeg" alt="slide 7">\
+                      <img class="d-block mx-auto" style="width: 900px; height:450px;" src="https://img.europapress.es/fotoweb/fotonoticia_20121211215221_640.jpg" alt="slide 7">\
                       <div class="carousel-caption d-none d-md-block" id="carouseltext">\
                           <h5>Seven: Los siete pecados capitales</h5>\
                           <p>(1995)</p>\
                         </div>\
                     </div>\
                     <div class="carousel-item">\
-                        <img class="d-block mx-auto" style="width: 900px; height:450px;" src="http://img.fenixzone.net/i/IkFhqAO.jpeg" alt="slide 8">\
+                        <img class="d-block mx-auto" style="width: 900px; height:450px;" src="https://www.bolsamania.com/cine/wp-content/uploads/2017/08/88-1-600x400.jpg" alt="slide 8">\
                         <div class="carousel-caption d-none d-md-block" id="carouseltext">\
                             <h5>Terminator 2: El juicio final</h5>\
                             <p>(1991)</p>\
                           </div>\
                       </div>\
                       <div class="carousel-item">\
-                          <img class="d-block mx-auto" style="width: 900px; height:450px;" src="http://img.fenixzone.net/i/3jcLAXp.jpeg" alt="slide 9">\
+                          <img class="d-block mx-auto" style="width: 900px; height:450px;" src="https://www.bfi.org.uk/sites/bfi.org.uk/files/styles/full/public/image/trainspotting-1996-008-ewan-bremner-ewan-mcgregor-robert-carlyle-00m-m63.jpg?itok=tmpxRcqP" alt="slide 9">\
                           <div class="carousel-caption d-none d-md-block" id="carouseltext">\
                               <h5>Trainspotting</h5>\
                               <p>(1996)</p>\
                             </div>\
                         </div>\
                         <div class="carousel-item">\
-                            <img class="d-block mx-auto" style="width: 900px; height:450px;" src="http://img.fenixzone.net/i/Zh4Due0.jpeg" alt="slide 10">\
+                            <img class="d-block mx-auto" style="width: 900px; height:450px;" src="https://cdn.onebauer.media/one/empire-images/articles/5cd5741b133d503e3a49a5c9/matrix-reloaded.jpg?quality=50&width=1800&ratio=16-9&resizeStyle=aspectfill&format=jpg" alt="slide 10">\
                             <div class="carousel-caption d-none d-md-block" id="carouseltext">\
                                 <h5>Matrix</h5>\
                                 <p>(1999)</p>\
